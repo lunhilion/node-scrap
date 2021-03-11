127c664dc98139178fc040ab59ecf209bd8ec91e
Enrico Sanguin <e.sanguin.92@gmail.com>
2021-03-11 19:30:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.post("/scrap", jsonParser, async (request, response) => {
     const browser = await puppeteer.launch({
       args: ["--no-sandbox"]
     });
+    
     const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0); 
     await page.goto(
       "https://i.360.cn/login/?src=pcw_home&destUrl=https://www.360.cn/"
     );
@@ -30,7 +32,7 @@ app.post("/scrap", jsonParser, async (request, response) => {
     response.json(elemText);
     await browser.close();
   } catch (error) {
-    console.log(error);
+    response.json(error);
   }
 });
 
